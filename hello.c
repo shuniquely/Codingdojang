8bf3e7d2fa5c791807e2a7f3aa8f3594937e9a77
shuniquely <shuniquely@gmail.com>
2020-07-01 17:17:31 +0900

    [C] Unit 3. Hello, World!

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%s\n", "hello, world!");
+    printf("%s, %s\n", "Hello", "1234");
+
+}
---
+++ practice_hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+    printf("%s", "1234567890");
+}
